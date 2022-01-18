48af2e9cdc0bbffb1f36fdf7359661449a8c8586
Surya2128 <97678526+Surya2128@users.noreply.github.com>
2022-01-18 15:23:02 +0530

    my first commit

---
+++ src/gitDemoProjectVersion1/Test1.java
@@ -0,0 +1,19 @@
+package gitDemoProjectVersion1;
+
+public class Test1 {
+ 
+		
+		public static void main(String[]args)
+		{
+		String str ="india is my";
+		String rev="";
+
+		String sp[]=str.split (rev);
+		for(int a=str.length()-1; a>=0; a--)
+		{
+		rev =rev+""+sp [a];
+		}
+		System.out.println(rev);
+		}
+
+}
